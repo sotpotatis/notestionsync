96b69682025a0121ec98beddceb26eb07489f0eb
sotpotatis <albin@albins.website>
2023-01-21 21:37:26 +0100

    Update default directory

--- systemd/run_notestion_sync.sh
+++ systemd/run_notestion_sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #run_notestion_sync.sh
 #Runs the Google Drive to Notion syncing script.
-SCRIPT_LOCATION_DIR="$HOME/Documents/notestionsync"
+SCRIPT_LOCATION_DIR="$HOME/notestionsync"
 echo "Moving into directory..."
 cd $SCRIPT_LOCATION_DIR || exit 1
 echo "Running script..."
